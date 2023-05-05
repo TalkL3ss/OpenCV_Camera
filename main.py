60e04505617ed19f81311a7cde2f34fc986448b2
TalkL3ss <30426929+TalkL3ss@users.noreply.github.com>
2023-05-05 21:15:45 +0300

    Update main.py
    Bug Fix, fix record on motion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import cv2
 import time
 from datetime import datetime
@@ -8,28 +8,29 @@ app = Flask(__name__)
 
 # Set up camera
 global filename
-camera = cv2.VideoCapture(1)
-time.sleep(2) # Warm up camera
+camera = cv2.VideoCapture(0)
+time.sleep(3)  # Warm up camera
 frame_width = int(camera.get(3))
 frame_height = int(camera.get(4))
 video_writer = None
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 filename = None
 start_record = False
 file_set = False
-
+stop_rec = 30
 
 # Set up servo motor
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(11, GPIO.OUT)
-#pwm = GPIO.PWM(11, 50)
-#pwm.start(0)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(11, GPIO.OUT)
+# pwm = GPIO.PWM(11, 50)
+# pwm.start(0)
 
 # Set up motion detection
-motion_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=500,detectShadows=True)
+motion_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=50, detectShadows=False)
 
 # Set up video writer
 recording_start_time = None
+
 
 # Main route
 @app.route('/')
@@ -48,7 +49,7 @@ def index():
 </head>
 <body>
     <div id="video-stream">
-        <img src="{{ url_for('video_feed') }}" style="width: 640px; height: 480px;">
+        <img src="{{ url_for('video_feed') }}" style="width: 1024px; height: 768px;">
     </div>
     <div id="servo-control">
         <input type="range" min="0" max="180" value="90" class="slider" id="servo-angle">
@@ -92,123 +93,70 @@ def index():
 </body>
 </html>""")
 
+
 # Video feed route
 def gen():
-    print("I'm Here")
-    global recording_start_time, start_record
-    start_record=False
-    while True:
-        ret, frame = camera.read()
-        if not ret:
-            break
-        else:
-            # Apply motion detection
-            fg_mask = motion_detector.apply(frame)
-            fg_mask = cv2.threshold(fg_mask, 200, 255, cv2.THRESH_BINARY)[1]
-            fg_mask = cv2.erode(fg_mask, None, iterations=2)
-            fg_mask = cv2.dilate(fg_mask, None, iterations=2)
-            contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            for c in contours:
-                # Only record video when motion is detected
-                if recording_start_time is None:
-                    print('Motion detected')
-                    if not start_record:
-                      filename = datetime.now().strftime('recordings\%Y-%m-%d_%H-%M.mp4')
-                      start_record=True
-                    video_writer = cv2.VideoWriter(filename, fourcc, 20.0, (frame_width, frame_height))
-                    recording_start_time = time.time()
-                # Draw bounding box around moving object
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                video_writer.write(frame)
-            # Save video for 10 seconds after motion stops
-            if recording_start_time is not None and time.time() - recording_start_time >= 10:
-                print('Motion stopped')
-                video_writer.release()
-                recording_start_time = None
-                start_record=False
-            # Convert frame to JPEG and yield it to the video feed
-            ret, jpeg = cv2.imencode('.jpg', frame)
-            frame = jpeg.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    global filename
+    i = 0
+    start_movement_time = 0
+    while camera.isOpened():
+        # to read frame by frame
+        _, img_1 = camera.read()
+        _, img_2 = camera.read()
 
-def gen2():
- global filename
- i = 0
- start_movment_time=0
- while camera.isOpened():
-    # to read frame by frame
-     _, img_1 = camera.read()
-     _, img_2 = camera.read()
+        # find difference between two frames
+        diff = cv2.absdiff(img_1, img_2)
 
-    # find difference between two frames
-     diff = cv2.absdiff(img_1, img_2)
+        # to convert the frame to grayscale
+        diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
-    # to convert the frame to grayscale
-     diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        # apply some blur to smoothen the frame
+        diff_blur = cv2.GaussianBlur(diff_gray, (5, 5), 0)
 
-    # apply some blur to smoothen the frame
-     diff_blur = cv2.GaussianBlur(diff_gray, (5, 5), 0)
+        # to get the binary image
+        _, thresh_bin = cv2.threshold(diff_blur, 25, 255, cv2.THRESH_BINARY)
 
-    # to get the binary image
-     _, thresh_bin = cv2.threshold(diff_blur, 20, 255, cv2.THRESH_BINARY)
+        # to find contours
+        contours, hierarchy = cv2.findContours(thresh_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if contours != () and filename is None:
+            print("New name")
+            start_movement_time = time.time()
+            filename = datetime.now().strftime('recordings\\%Y-%m-%d_%H-%M-%S.mp4')
+            video_writer = cv2.VideoWriter(filename, 0x7634706d, 70, (frame_width, frame_height))
+            print("Start")
+        elif contours == () and (time.time() - start_movement_time) >= stop_rec and i >= 200:
+            video_writer.release()
+            start_movement_time = 0
+            filename = None
+            print("Stop")
+            i = 0
+        elif contours == () and (time.time() - start_movement_time) >= stop_rec and filename is not None:
+            print(i)
+            video_writer.write(img_1)
+            i = i + 1  # frame has been made in the frame
+            # to draw the bounding box when the motion is detected
 
-    # to find contours
-     contours, hierarchy = cv2.findContours(thresh_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-     if contours == ():
-      #print(i)
-      i=i+1
-      if i >= 6:
-       if start_movment_time != 0 and (time.time() - start_movment_time) >= 30 and filename is not None:
-         video_writer.release()
-         start_movment_time = 0
-         start_record=False
-         filename = None
-         print("Stop")
-       elif start_movment_time !=0 and filename is None:
-          #start_record=False
-          #file_set = True
-          filename = datetime.now().strftime('recordings\%Y-%m-%d_%H-%M-%S.avi')
-          video_writer = cv2.VideoWriter(filename, fourcc, 100, (frame_width, frame_height))
-          print("Start")
-       elif filename is not None:
-          i=0	   
-      elif i < 20 and start_movment_time != 0 and (time.time() - start_movment_time) < 30 and filename is not None:
-       video_writer.write(img_1)
-	  #print(contours)
-      #i=0
+        for contour in contours:
+            if cv2.contourArea(contour) >= 300:
+                cv2.rectangle(img_1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            if filename is not None:
+                video_writer.write(img_2)
+            i = 0
+            x, y, w, h = cv2.boundingRect(contour)
 
-       
-    # to draw the bounding box when the motion is detected
-     for contour in contours:
-         if cv2.contourArea(contour) >= 1:
-             cv2.rectangle(img_1, (x, y), (x+w, y+h), (0, 255, 0), 2)
-         if filename is not None:
-           video_writer.write(img_2)
-         if start_movment_time ==0:
-          start_movment_time=time.time()
-          
-         #print("i=0")
-         i=0
-         x, y, w, h = cv2.boundingRect(contour)
-
-    # cv2.drawContours(img_1, contours, -1, (0, 255, 0), 2)
-    # display the output
-     #cv2.imshow("Detecting Motion...", img_1)
-     ret, jpeg = cv2.imencode('.jpg', img_1)
-    # if start_movment_time != 0:
-	  
-     frame = jpeg.tobytes()
-     yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # display the output
+        ret, jpeg = cv2.imencode('.jpg', img_1)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 # Video feed endpoint
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen2(),
+    return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 # Servo control endpoint
 @app.route('/move', methods=['POST'])
@@ -217,11 +165,13 @@ def move():
     pwm.ChangeDutyCycle(duty_cycle)
     return 'ok'
 
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
 # Clean up when done
+video_writer.release()
 camera.release()
-#cv2.destroyAllWindows()
-#pwm.stop()
-#GPIO.cleanup()
+# cv2.destroyAllWindows()
+# pwm.stop()
+# GPIO.cleanup()
